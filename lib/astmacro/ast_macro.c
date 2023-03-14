6ae790bf4d92314659183b7176a3b765fbc2c604
MarcFaussurier <faussurier.marc@icloud.com>
2023-03-14 09:02:18 +0100

    d_quotes

--- lib/astmacro/ast_macro.c
+++ lib/astmacro/ast_macro.c
@@ -1,13 +1,14 @@
 #include "ast_macro.h"
 
 DEF_LIST(macro, macro_list, 0);
-DEF_KLIST(ast_list *, ast_match_list, 0);
 
 int apply_macro(ast_list **list, macro_list *macros)
 {
 	macro_list	*m;
 	ast_list	*a;
+	ast_list	*root;
 
+	root = 0;
 	a = *list;
 	while (a)
 	{
@@ -18,7 +19,10 @@ int apply_macro(ast_list **list, macro_list *macros)
    		        return 1;
 			m = m->next;
         }
+		if (!root)
+			root = a;
 		a = a->next;
 	}
+	*list = root;
     return 0;
 }
--- lib/astmacro/test.c
+++ lib/astmacro/test.c
@@ -4,24 +4,33 @@ int m_d_quote(ast_list	**l)
 {
 	ast_list    *n;
     char        *addr;
+    int count = 0;
 
     n = *l;
 	if (strcmp(n->data->type, "raw"))
         return 0;
-    addr = strchr(n->data->value, "\"");
+    addr = strchr(n->data->value, '"');
     if (!addr)
         return 0;
-    while (*addr && !(*(addr - 1) != '\\'&& (*addr == '\"')))
+    count += 1;
+    addr += 1;
+    while (*addr && !(*(addr - 1) != '\\'&& (*addr == '"')))
     {
+        count += 1;
         addr += 1;
     }
+    if (*addr)
+        addr += 1;
+    printf("ssdd\n");
     ast_list *prev = (*l)->prev;
     ast_list *next = (*l)->next;
-    ast_list *new_node = ast_list_new(alloc(ast_value, .type = "raw", .value = strndup(n->data->value, addr - n->data->value)));
+    ast_list *new_node = ast_list_new(alloc(ast_value, .type = "raw", .value = strndup(n->data->value, addr - n->data->value - count - 1)));
     new_node->prev = prev;
-    new_node->next = ast_list_new(alloc(ast_value, .type = "D_QUOTES", .value = strndup(addr + (addr - n->data->value))));
+    new_node->next = ast_list_new(alloc(ast_value, .type = "D_QUOTES", .value = strndup(addr - count, count - 1)));
     new_node->next->next = ast_list_new(alloc(ast_value, .type = "raw", .value = strdup(addr)));
     new_node->next->next->next = next;
+
+    *l = new_node;
 
     return 0;
 }
@@ -33,7 +42,7 @@ int main()
 
 	l = 0;
 	
-	ast = parse("\"he\\yyo\" ab a ");
+	ast = parse("test\"he\\yyo\" ab a ");
 	macro_list_add(&l, macro_list_new(m_d_quote));
 	apply_macro(&ast, l);
 	ast_dump(ast);
